77b8f0f685421f4de54af5c4a51aea6f5c6fc2dc
Mehak <mehakkanwal8@gmail.com>
2021-12-20 16:25:35 +0500

    encryption decryption file added

---
+++ EncryptionDecryption.py
@@ -0,0 +1,36 @@
+def encryption(word, shift):
+    result = ""
+
+    for i in range(len(word)):
+        char = word[i]
+
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        else:
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+    return result
+
+
+def decryption(word, shift):
+    result = ""
+
+    for i in range(len(word)):
+        char = word[i]
+
+        if char.isupper():
+            result += chr((ord(char) - shift - 65) % 26 + 65)
+        else:
+            result += chr((ord(char) - shift - 97) % 26 + 97)
+    return result
+
+
+# check the above function
+text = "DEFENDTHEFORT"
+s = 7
+
+encrypt = encryption(text, s)
+decrypt = decryption(encrypt, s)
+
+print("Original text: ", text)
+print("Encrypted: ", encrypt)
+print("Decrypted: ", decrypt)
